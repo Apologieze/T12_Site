3905c14b9042ba5bd7d1dca2e167e1d6815f8270
Apologieze <apoloing2@gmail.com>
2023-09-09 17:20:54 +0200

    First commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import Card from "../card.svelte";
+</script>
+<AppBar>(title)</AppBar>
+<div class="box container h-screen mx-auto flex justify-between items-center flex-col gap-6">
+	<h1 class="h1">T12 ESPORT</h1>
+	<h2 class="h2">Feur</h2>
+	<Card></Card>
+</div>
