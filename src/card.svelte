15f8f3cc972872f74514003b6dce457d6387651c
Apologieze <apoloing2@gmail.com>
2023-09-10 01:17:00 +0200

    Début du site avec carte

--- src/card.svelte
+++ src/card.svelte
@@ -1,1 +1,11 @@
-<div class="card p-4 card-hover">Basic</div>
+<script lang="ts">
+    export let name: String;
+    export let src: String;
+</script>
+
+<div class="card p-4 card-hover flex flex-col w-1/4 gap-4">
+    <div class="title flex justify-center">
+        <h2 class="h2">{name}</h2>
+    </div>
+    <img {src} alt="image de la carte">
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,20 @@
 <script>
 	import Card from "../card.svelte";
+	import { AppBar } from '@skeletonlabs/skeleton';
 </script>
-<AppBar>(title)</AppBar>
-<div class="box container h-screen mx-auto flex justify-between items-center flex-col gap-6">
-	<h1 class="h1">T12 ESPORT</h1>
-	<h2 class="h2">Feur</h2>
-	<Card></Card>
+
+<div class="page h-screen">
+	<AppBar class="flex items-center justify-center" gap="gap-0" background="bg-surface-900/40">
+		<div class="box-header flex justify-center w-full">
+			<h1 class="h1">T12 ESPORT</h1>
+		</div>
+	</AppBar>
+
+	<div class="box container mx-auto flex items-center flex-col h-full gap-8 p-4">
+		<h2 class="h2">Feur</h2>
+		<div class="carte flex justify-center gap-10">
+			<Card name="Momozhok" src="/img/momozhok.jpg"></Card>
+			<Card name="Wataa" src="/img/wata.jpg"></Card>
+		</div>
+	</div>
 </div>
